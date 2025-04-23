4714100956a5af83b4e172b6d949751efda0c7a0
Lovince <lovince2710@gmail.com>
2025-04-23 22:27:04 +0530

    Update BASH1.py

--- BASH1.py
+++ BASH1.py
@@ -1,2 +1,7 @@
 git clone https://github.com/your-username/LovinceAI.git
 cd LovinceAI
+
+cd backend
+export X_BEARER_TOKEN="your_bearer_token_here"
+export NEWS_API_KEY="your_news_api_key_here"
+python grok_lovince_unified_core_with_ml.py
