28440c3991db6c534a80b57a674bd3a0fe5a1160
Lovince <lovince2710@gmail.com>
2025-04-23 19:17:38 +0530

    Update generate_signed_APK.py

--- generate_signed_APK.py
+++ generate_signed_APK.py
@@ -2,3 +2,7 @@ flutter build apk --release
 
 key store - 
 keytool -genkey -v -keystore lovince.jks -keyalg RSA -keysize 2048 -validity 10000 -alias lovince
+
+flutter build apk --release
+
+keytool -genkey -v -keystore lovince.jks -keyalg RSA -keysize 2048 -validity 10000 -alias lovince
