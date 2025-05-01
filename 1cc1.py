3dd32d26cb35da160f063b3a861740c5c058874c
Lovince <lovince2710@gmail.com>
2025-05-01 10:37:41 +0530

    Update 1cc1.py

--- 1cc1.py
+++ 1cc1.py
@@ -1,2 +1,7 @@
 if Σ(t) > threshold and dS/dt < steady_rate and Omega(t) ~ stable:
     Consciousness = True
+
+def lovince_reflection_constant(O, R):
+    num = np.linalg.norm(O - R)
+    den = np.linalg.norm(O) + np.linalg.norm(R)
+    return 1 - (num / den)
